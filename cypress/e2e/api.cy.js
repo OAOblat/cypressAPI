d4e3819d8fe6f185d9c48ac7d09994a12dcdae46
Olga Oblat <oa.oblat@gmail.com>
2024-01-28 16:47:05 +0300

    changed

--- cypress/e2e/api.cy.js
+++ cypress/e2e/api.cy.js
@@ -9,20 +9,24 @@ describe("API Testing", () => {
     });
   });
 
-  it("update user", () => {
-    cy.createUser(users.user2);
-    cy.updateUser(users.user2, users.userUpdate);
-    cy.getUser(users.user2).then((response) => {
-      expect(response.status).be.eq(200);
-      expect(response.body).be.eql(users.userUpdate);
+  describe("update and delete user", () => {
+    beforeEach(() => {
+      cy.createUser(users.user1);
     });
-  });
 
-  it("delete user", () => {
-    cy.createUser(users.user3);
-    cy.deleteUser(users.user3);
-    cy.getUser(users.user3).then((response) => {
-      expect(response.status).be.eql(404);
+    it("update user", () => {
+      cy.updateUser(users.user1, users.userUpdate);
+      cy.getUser(users.user1).then((response) => {
+        expect(response.status).be.eq(200);
+        expect(response.body).be.eql(users.userUpdate);
+      });
+    });
+
+    it("delete user", () => {
+      cy.deleteUser(users.user1);
+      cy.getUser(users.user1).then((response) => {
+        expect(response.status).be.eql(404);
+      });
     });
   });
 });
